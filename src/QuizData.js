9827b5882cf56a4df73e571f47a7db39809064b6
Antonio Marcelino <52008474+antoniomarcelino@users.noreply.github.com>
2020-08-14 00:08:10 -0300

    1st

---
+++ src/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `What is the capital of Brazil?`,
+        options: [`New Delhi`, `Brazilia`, `Aba`, `Onisha`],
+        answer: `Brazilia`
+    },
+    {
+        id: 1,
+        question: `What is the capital of Portugal?`,
+        options: [`Lisboa`, `Kyoto`, `Owerri`, `Enugu`],
+        answer: `Lisboa`
+    },
+    {
+        id: 2,
+        question: `What is the capital of UK?`,
+        options: [`Mumbai`, `Abuja`, `London`, `Sydney`],
+        answer: `London`
+    },
+    {
+        id: 3,
+        question: `What is the capital of Japan?`,
+        options: [`Bangalore`, `Tokyo`, `Istanbul`, `Enugu`],
+        answer: `Tokyo`
+    },
+    {
+        id: 4,
+        question: `What is the capital of Syria?`,
+        options: [`Rimadi`, `Damascus`, `Owerri`, `Tel Aviv`],
+        answer: `Damascus`
+    },
+]
